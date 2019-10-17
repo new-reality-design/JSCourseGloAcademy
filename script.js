54e4b72e6ad60383a095c6731c29d73aaf7e15ad
OlgaB <olga.bereznaja@gmail.com>
2019-10-17 16:20:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let money = 'Зарплата в фунтах каждый месяц должна быть (£):';
+let income = 1000;
+let addExpenses = 'Чтобы платить ипотеку каждый месяц- (£):';
+let deposit = 250;
+let mission = 'Tогда можно выплатить за свою квартиру ';
+let period = 'через 20 лет';
+//Рыночных цен не знаю, цифры взяты наобум :)
+
+alert('Нажми ОК чтобы прочитать текст в консоли:');
+
+console.log(money);
+console.log(income);
+console.log(addExpenses);
+console.log(deposit);
+console.log(mission + period);
