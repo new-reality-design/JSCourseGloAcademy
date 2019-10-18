59ae7cff69d59dcc3a2e1297e40fc84e921da92a
OlgaB <olga.bereznaja@gmail.com>
2019-10-18 18:05:32 +0100

    Закончено второе задание

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-let money = 'Зарплата в фунтах каждый месяц должна быть(£)- :';
-let income = 1000;
-let addExpenses = 'Чтобы платить ипотеку каждый месяц(£)- :';
-let deposit = 250;
-let mission = 'Tогда можно выплатить за свою квартиру ';
-let period = 'через 20 лет';
+let money = 1200; //Доход в месяц
+let income = 'Овертайм, помощь соседям, присмотр за животными';
+let addExpenses = 'Коммунальные услуги. Покупки. Транспорт. Одежда';
+let deposit = true;
+let mission = 3000; //Сумма к накоплению
+let period = 12; //Период в месяцах
 
-alert('Нажми ОК чтобы прочитать текст в консоли:');
+//1
+console.log('Типы данных в переменных-');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log(money);
-console.log(income);
-console.log(addExpenses);
-console.log(deposit);
-console.log(mission + period);
+//2
+console.log('Длина строки "income"-');
+console.log(income.length);
+
+//3
+console.log('За период длиной в ' + period + ' месяцев-');
+console.log('я хочу накопить ' + mission + ' фунтов.');
+
+//4
+console.log('"addExpenses" в нижнем регистре-');
+console.log(addExpenses.toLowerCase());
+console.log('"addExpenses" в виде массива-');
+console.log(addExpenses.split('. '));
+//console.log('В одну строку-');
+//console.log(addExpenses.toLowerCase(), addExpenses.split('. '));
+
+//5
+let budgetDay = money / 30;
+console.log('Дневной бюджет и результат-');
+console.log(budgetDay);
+console.log(money % 30);
