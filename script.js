68085a1ee36566e4a19fa7242b23d1585fac468a
OlgaB <olga.bereznaja@gmail.com>
2019-10-22 16:15:48 +0100

    Подчищены логи

--- script.js
+++ script.js
@@ -35,21 +35,15 @@ let monthlyExpenses1 = prompt(
   'Какие обязательные ежемесячные расходы у вас есть?'
   //Бытовые расходы - 600
 );
-console.log('monthlyExpenses1: ', monthlyExpenses1);
 let totalExpenses1 = prompt('Во сколько это обойдется?');
-console.log('totalExpenses1: ', totalExpenses1);
 let totalExpensesNum1 = parseFloat(totalExpenses1);
-console.log('totalExpensesNum1: ', totalExpensesNum1);
 
 let monthlyExpenses2 = prompt(
   'Какие другие обязательные ежемесячные расходы у вас есть?'
   //Банковский депозит - 200
 );
-console.log('monthlyExpenses2: ', monthlyExpenses2);
 let totalExpenses2 = prompt('Во сколько это обойдется?');
-console.log('totalExpenses2: ', totalExpenses2);
 let totalExpensesNum2 = parseFloat(totalExpenses2);
-console.log('totalExpensesNum2: ', totalExpensesNum2);
 
 //6
 let budgetMonth = money - (totalExpensesNum1 + totalExpensesNum2);
@@ -58,7 +52,8 @@ console.log('доход за месяц: ', budgetMonth);
 //7
 let mission = 5000; //Сумма к накоплению
 
-let finishMission = mission / budgetMonth; //посчитать за сколько месяцев будет достигнута цель mission
+//Посчитать за сколько месяцев будет достигнута цель mission
+let finishMission = mission / budgetMonth;
 console.log('finishMission: ', Math.ceil(finishMission));
 
 //8
@@ -66,7 +61,7 @@ let budgetDay = budgetMonth / 30;
 let budgetDayRound = Math.floor(budgetDay);
 console.log('budgetDay: ', budgetDayRound);
 
-//9  (от 50 до 0 фунтов в день.)
+//9  (Остаток- от 50 до 0 фунтов в день.)
 if (budgetDayRound >= 50) {
   console.log('“Высокий уровень дохода”');
 } else if (budgetDayRound >= 25 && budgetDayRound < 50) {
