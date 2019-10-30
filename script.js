4179b30c2c88f2a392750a11d6743615a6fd8bbd
OlgaB <olga.bereznaja@gmail.com>
2019-10-30 20:25:47 +0000

    adding function

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
 'use strict';
 
-let money;
-
-let start = function() {
-  do {
-    money = prompt('Ваш месячный доход? Укажите сумму в цифрах.', '50000');
-  } while (isNaN(money) || money === '' || money === null);
-};
+let money,
+  start = function() {
+    do {
+      money = prompt('Ваш месячный доход? Укажите сумму в цифрах.', '50000');
+    } while (isNaN(money) || money === '' || money === null);
+  };
 
 start();
 
-let addExpenses = prompt(
-    'Перечислите возможные расходы за рассчитываемый период через запятую.',
-    'Бытовые расходы, Транспорт'
-  ),
-  deposit = confirm('Есть ли у вас депозит в банке?'),
-  income = 'Фриланс, парт-тайм',
-  mission = 150000, //Сумма к накоплению
-  period = 12; //Период в месяцах
+let appData = {
+  income: {},
+  addIncome: [], //Доп.доходы
+  expenses: {}, //Доп.расходы
+  addExpenses: [], //Возможные расходы
+  deposit: false,
+  mission: 150000,
+  period: 6,
+  budget: money,
+  budgetDay: 0,
+  expensesMonth: 0,
+  budgetMonth: 0,
+  asking: function() {
+    //расспрос пользователя именно тут
+    let addExpenses = prompt(
+      'Перечислите возможные расходы за рассчитываемый период через запятую.',
+      'Бытовые расходы, Транспорт'
+    );
+    appData.addExpenses = addExpenses.toLowerCase().split(',');
+    appData.deposit = confirm('Есть ли у вас депозит в банке?');
+  },
+  getExpensesMonth: function() {
+    let sum = 0;
+    let sum1, sum2, monthlyExpenses1, monthlyExpenses2;
+    for (let i = 0; i < 2; i++) {
+      if (i === 0) {
+        monthlyExpenses1 = prompt(
+          'Введите обязательную статью расходов?',
+          'Транспорт и квартплата'
+        );
+        do {
+          sum1 = +prompt('Во сколько это обойдется?', '15000');
+        } while (isNaN(sum1) || sum1 === '' || sum1 === 0 || sum1 === null);
+      }
+      if (i === 1) {
+        monthlyExpenses2 = prompt(
+          'Введите обязательную статью расходов?',
+          'Расходы на питание'
+        );
+        do {
+          sum2 = +prompt('Во сколько это обойдется?', '10000');
+        } while (isNaN(sum2) || sum2 === '' || sum2 === 0 || sum2 === null);
+      }
+    }
+    sum += sum1 + sum2;
+    return sum;
+  }
+};
+//4) Функции getExpensesMonth, getAccumulatedMonth, getTargetMonth, getStatusIncome - сделать методами объекта AppData
+//income = 'Фриланс, парт-тайм', ?????
+//mission = 150000, //Сумма к накоплению ?????
+//period = 12; //Период в месяцах ?????
 
-const showTypeOf = function(data) {
+/*const showTypeOf = function(data) {
   console.log(data, typeof data);
 };
 showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
+showTypeOf(appData.income);
+showTypeOf(appData.deposit);*/
 
-let monthlyExpenses1, monthlyExpenses2;
+//let monthlyExpenses1, monthlyExpenses2;
 
 //Сумма расходов за месяц
-const getExpensesMonth = function() {
+/*function getExpensesMonth() {
   let sum = 0,
     sum1,
     sum2;
-
   for (let i = 0; i < 2; i++) {
     //Временное условие x 2
     if (i === 0) {
@@ -57,13 +99,13 @@ const getExpensesMonth = function() {
   }
   sum += sum1 + sum2;
   return sum;
-};
+} */
 
 let expensesAmount = getExpensesMonth(); //Тут- результ. вызова Ф.
-let budgetMonth = money - expensesAmount;
+let budgetMonth = money - appData.expensesAmount;
 
 //Посчитать за сколько месяцев будет достигнута цель mission
-let finishMission = mission / budgetMonth,
+let finishMission = appData.mission / budgetMonth,
   budgetDay = budgetMonth / 30,
   budgetDayRound = Math.floor(budgetDay);
 
@@ -76,7 +118,7 @@ let accumulatedMonth = getAccumulatedMonth();
 
 //За какой период будет достигнута цель
 function getTargetMonth() {
-  let getSum = mission / accumulatedMonth;
+  let getSum = appData.mission / accumulatedMonth;
   if (getSum <= 0) {
     console.log('“Цель не будет достигнута.”');
   } else {
@@ -88,9 +130,9 @@ function getTargetMonth() {
 getTargetMonth();
 
 //Накопления за "период" - число месяцев произвольное
-period = 4;
-console.log('Накопления за период в ' + period + ' месяца/цев:');
-console.log(accumulatedMonth * period);
+appData.period = 4;
+console.log('Накопления за период в ' + appData.period + ' месяца/цев:');
+console.log(accumulatedMonth * appData.period);
 
 //Вывод-
 const getStatusIncome = function() {
