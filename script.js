3ee276a3b3786d1986ca0b04e2e3ade87521926a
OlgaB <olga.bereznaja@gmail.com>
2019-11-01 15:30:49 +0000

    чтото наконецто получилось

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ let money,
 start();
 
 let appData = {
+  budget: money, //0,
   income: {},
   addIncome: [], //Доп.доходы
   expenses: {}, //Доп.расходы
   addExpenses: [], //Возможные расходы
   deposit: false,
-  mission: 150000,
+  mission: 100000,
   period: 6,
-  budget: 0,
   budgetDay: 0,
   expensesMonth: 0,
   budgetMonth: 0,
   asking: function() {
     appData.budget = money;
     let addExpenses = prompt(
-      'Перечислите возможные расходы за рассчитываемый период через запятую.',
-      'Бытовые расходы, Транспорт'
+      'Перечислите возможные расходы за рассчитываемый период через запятую:',
+      'Ремонт, лекарства, одежда, огненная вода, волшебные грибы.'
     );
     appData.addExpenses = addExpenses.toLowerCase().split(',');
     appData.deposit = confirm('Есть ли у вас депозит в банке?');
@@ -33,7 +33,7 @@ let appData = {
     for (let i = 0; i < 2; i++) {
       expenses1 = prompt(
         'Введите обязательную статью расходов?',
-        'Транспорт и квартплата'
+        'Карты, деньги, два ствола.'
       );
       do {
         question = +prompt('Во сколько это обойдется?', '10000');
@@ -46,35 +46,26 @@ let appData = {
       appData.expenses[expenses1] = question; //Куда.сохраняется[ЧтоСохраняется:Ключ]=ЧтоСохраняется:значение;
     }
   },
-  /////
+  //
   getExpensesMonth: function() {
     for (let key in appData.expenses) {
-      console.log('KeY', key);
+      //console.log('Ключ', key);//Проверка имени ключа
       appData.expensesMonth += +appData.expenses[key];
     }
   },
 
-  ////////ЧИСТЫЕ накопления за месяц (Доходы минус расходы) "getBudget"
+  //ЧИСТЫЕ накопления за месяц (Доходы минус расходы) "getBudget"
   //Бюджет на день=
   //Результат ЧИСТЫХ накоплений за месяц= поделённый на 30.
   getBudget: function() {
     appData.budgetMonth = appData.budget - appData.expensesMonth;
     appData.budgetDay = appData.budgetMonth / 30;
   },
-  //return money - appData.expensesMonth;
-  /**
-   * let budgetDay = appData.getAccumulatedMonth() / 30; //Бюджет на день= Результат
-//ЧИСТЫХ накоплений за месяц= поделённый на 30.
-   * 8) getAccumulatedMonth переименовать в getBudget. Этот метод будет считать budgetMonth и budgetDay (перенести эти команды в этот метод)
-   * //6
-let budgetMonth = money - (totalExpensesNum1 + totalExpensesNum2);
-console.log('доход за месяц: ', budgetMonth);
-   */
-  ////////За сколько месяцев будет достигнута цель? "getTargetMonth"
+  //За сколько месяцев будет достигнута цель? "getTargetMonth"
   getTargetMonth: function() {
-    return appData.mission / appData.budget;
+    return appData.mission / appData.budgetMonth;
   },
-  ///////”уровень дохода”. Оценка уровня дневного бюджета. "getStatusIncome"
+  //”уровень дохода”. Оценка уровня дневного бюджета. "getStatusIncome"
   getStatusIncome: function() {
     if (appData.budgetDay >= 800) {
       return '“Высокий уровень дохода”';
@@ -87,18 +78,15 @@ console.log('доход за месяц: ', budgetMonth);
     }
   }
 };
-///////Вызов методов appData.asking
+//Вызов методов appData.asking
 appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
 appData.getTargetMonth();
 appData.getStatusIncome();
 
-//////////
-console.log('Расходы за месяц: ' + appData.expensesMonth);
-
-//let budgetDay = appData.getBudget() / 30; //Бюджет на день= Результат
-//ЧИСТЫХ накоплений за месяц= поделённый на 30.
+//
+console.log('Расходы за месяц: ' + appData.expensesMonth); //Оставить
 
 //Посчитать за сколько месяцев будет достигнута цель накоплений- mission
 if (appData.getTargetMonth() > 0) {
@@ -107,13 +95,16 @@ if (appData.getTargetMonth() > 0) {
   );
 } else {
   console.log('“Цель не будет достигнута.”');
-}
+} //Оставить
 
 //Накопления за "любой период" - число месяцев произвольное
-console.log('Накопления за период в ' + appData.period + ' месяца/цев:');
-console.log(appData.budgetMonth * appData.period);
+// console.log('Накопления за период в ' + appData.period + ' месяца/цев:');
+// console.log(appData.budgetMonth * appData.period);//DISABLE
 
-//Вывод-
-//Тут была getStatusIncome
-console.log(appData.getStatusIncome());
-console.log(appData.money);
+//Результат уровня доходов
+console.log(appData.getStatusIncome()); //Оставить
+//console.log(appData);
+console.log('Наша программа включает в себя данные:');
+for (let key in appData) {
+  console.log('Свойства: ' + key + '   Значение: ' + appData[key]);
+}
